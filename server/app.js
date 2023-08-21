83974c818635a57db3c08697c60ba68db10474c5
shibu-sd <sd.shibu007@gmail.com>
2023-08-21 22:17:47 +0530

    added middleware

--- server/app.js
+++ server/app.js
@@ -1,26 +1,39 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+dotenv.config({path:'./.env'});
+require("./db/conn");
+
+const PORT = process.env.PORT;
+
+// Middleware
+const middleware = (req, res, next) => {
+    console.log("Hello from middleware");
+    next();
+}
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', middleware, (req, res) => {
     res.send("About");
-})
+});
 
 app.get('/contact', (req, res) => {
     res.send("Contact");
-})
+});
 
 app.get('/signin', (req, res) => {
     res.send("Signin");
-})
+});
 
 app.get('/signup', (req, res) => {
     res.send("Signup");
-})
+});
 
-app.listen(3000, () => {
-    console.log("Server started at port 3000");
-})
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
