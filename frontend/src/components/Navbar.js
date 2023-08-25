b0ca9e7df7bfdcaf6a1437270ceffe1cd03face6
shibu-sd <sd.shibu007@gmail.com>
2023-08-25 10:33:46 +0530

    added custom styles and logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
+import logo from "../images/logo3.png"
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <NavLink className="navbar-brand" to="#">Navbar</NavLink>
+                <NavLink className="navbar-brand" to="#"> <img src={logo} alt='logo' className='logo' /> </NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
