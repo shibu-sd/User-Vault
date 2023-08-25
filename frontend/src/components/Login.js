3e1220960300e43d42e44f91633dc5111b761be4
shibu-sd <sd.shibu007@gmail.com>
2023-08-25 20:34:47 +0530

    added styling to login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import logo from "../images/login1.jpg"
 
 const Login = () => {
   return (
     <div>
-        <h2>This is the login page</h2>
+
+      <div className="formbody form-container">
+
+        <img src={logo} alt='logo' />
+
+        <form className="fr">
+          <h1 className="elem">Login<br /> </h1>
+          <div className='item-container'>
+            <label htmlFor='email'> <i class="zmdi zmdi-email"></i> Email</label>
+            <input type="text" id="email" name="email" className="elem" placeholder='Enter email' />
+          </div>
+          <div className='item-container'>
+            <label htmlFor='email'> <i class="zmdi zmdi-lock-outline"></i> Password</label>
+            <input type="password" id="email" name="email" className="elem" placeholder='Enter password' />
+          </div>
+
+          <input type="button" value="Login" className="submit-btn elem mt-2" />
+
+        </form>
+
+      </div>
+
     </div>
   )
 }
