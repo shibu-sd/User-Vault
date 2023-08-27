20af2d3ff89ba78c88cf01a55e63084911ce83de
shibu-sd <sd.shibu007@gmail.com>
2023-08-27 19:42:59 +0530

    using fetch api in login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
 import logo from "../images/login1.jpg"
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:5000/signin", {
+      method : "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        email, password
+      })
+    });
+
+    const data = res.json();
+
+    if (res.status === 400 || !data){
+      window.alert("Invalid Credentials");
+    }else{
+      window.alert("Login Successful");
+      navigate('/');
+    }
+  }
+
   return (
     <div>
 
@@ -9,18 +39,18 @@ const Login = () => {
 
         <img src={logo} alt='logo' />
 
-        <form className="fr">
+        <form method='POST' className="fr">
           <h1 className="elem">Login<br /> </h1>
           <div className='item-container'>
             <label htmlFor='email'> <i class="zmdi zmdi-email"></i> Email</label>
-            <input type="text" id="email" name="email" className="elem" placeholder='Enter email' />
+            <input type="text" id="email" name="email" className="elem" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter email' />
           </div>
           <div className='item-container'>
             <label htmlFor='email'> <i class="zmdi zmdi-lock-outline"></i> Password</label>
-            <input type="password" id="email" name="email" className="elem" placeholder='Enter password' />
+            <input type="password" id="email" name="email" className="elem" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter password' />
           </div>
 
-          <input type="button" value="Login" className="submit-btn elem mt-2" />
+          <input type="submit" value="Login" onClick={loginUser} className="submit-btn elem mt-2" />
 
         </form>
 
